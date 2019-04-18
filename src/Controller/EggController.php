e8f6c58e3980fbe3d5c26c6bf3edaa4c89b2ccf4
Noel An <noel.an@hotmail.fr>
2019-04-18 17:19:21 +0200

    on est bon

--- src/Controller/EggController.php
+++ src/Controller/EggController.php
@@ -21,21 +21,5 @@ class EggController extends AbstractController
         // or
         // Send request https://foo.com/api/test?key=maKey&name=toto
 
-<<<<<<< HEAD
-	public function loki()
-	{
-		$client = new \GuzzleHttp\Client(['base_uri' => 'http://easteregg.wildcodeschool.fr/api/']	);
-		$response = $client->request('GET', 'eggs/5cac51240d488f0da6151bce', [
-       'loki'  => '1',
-   		]);
-   		$body = $response->getBody();
-		$body = $body->getContents();
-        $egg = json_decode($body);
 
-        return $this->twig->render('Egg/egg.html.twig', ['tableau' => $egg]);
-	}
-=======
-        return $this->twig->render('Egg/egg.html.twig', ['tableau' => $egg]);
-    }
->>>>>>> f910d7943743997afa906b50bf0dce9d4a01c733
 }
